c4315204377eabc2405b425c83cf29197960ac81
Nikolai Pashkevich <kulex4@gmail.com>
2015-04-04 22:14:37 +0300

    startup listener for home presenter

--- src/main/java/com/analitics/managerialstaff/ui/presenter/navigations/HomePresenter.java
+++ src/main/java/com/analitics/managerialstaff/ui/presenter/navigations/HomePresenter.java
@@ -1,11 +1,16 @@
 package com.analitics.managerialstaff.ui.presenter.navigations;
 
 import com.analitics.managerialstaff.ui.common.AbstractPresenter;
+import com.analitics.managerialstaff.ui.common.Action;
+import com.analitics.managerialstaff.ui.common.StartupFilter;
 import com.analitics.managerialstaff.ui.view.navigations.HomeView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.annotation.VaadinComponent;
 import org.vaadin.spring.annotation.VaadinUIScope;
+import org.vaadin.spring.events.Event;
 import org.vaadin.spring.events.EventBus;
+import org.vaadin.spring.events.EventScope;
+import org.vaadin.spring.events.annotation.EventBusListenerMethod;
 
 import java.io.Serializable;
 
@@ -19,5 +24,10 @@ public class HomePresenter extends AbstractPresenter<HomeView> implements Serial
     @Autowired
     public HomePresenter(HomeView view, EventBus.UIEventBus eventBus) {
         super(view, eventBus);
+    }
+
+    @EventBusListenerMethod(scope = EventScope.SESSION, filter = StartupFilter.class)
+    public void onStartup(Event<Action> event) {
+
     }
 }
