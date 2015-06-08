1cbe885b3f7bd990f048dde4e16412037df3d243
Nikolai Pashkevich <kulex4@gmail.com>
2015-06-08 22:03:40 +0300

    polish in pom file, home view label text

--- src/main/java/com/analitics/managerialstaff/ui/view/navigations/HomeViewImpl.java
+++ src/main/java/com/analitics/managerialstaff/ui/view/navigations/HomeViewImpl.java
@@ -19,13 +19,11 @@ public class HomeViewImpl extends VerticalLayout implements HomeView {
     @PostConstruct
     private void init() {
         MHorizontalLayout homeContent = new MHorizontalLayout(
-                new Label("Home view")
+                new Label("Программная поддержка анализа деятельности управленческого персонала»  предназначена для усовершенствования процесса анализа деятельности сотрудников и руководителей подразделений, с помощью математических методов экономического анализа. Программный продукт полезен в  решении таких вопросов, как индивидуальный отбор, аттестация и ротация управленческого  персонала.")
         ).withFullHeight().withFullWidth().withMargin(true);
         addComponent(homeContent);
     }
 
     @Override
-    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
-
-    }
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {}
 }
